d29a2ff74879f1b61846315f392b96bcd6baa3ab
myunnenggg <kanokporn.aub@dome.tu.ac.th>
2023-06-01 01:15:31 +0700

    add template

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 import express from 'express'; //สร้างตัวแปรเพื่อเรียกใช้งาน express
+const app = express();
+
 import chalk from 'chalk'; //ประกาศตัวช็อกที่ลงเพิ่ม
+
 import Debug from 'debug'; //เอาไว้ดูการทำงานว่าเกิดอะไรขึ้นบ้าง
 const debug = Debug('app');
 //const debug = require('debug')('app')
+
 import morgan from 'morgan'; // เป็น middleware?ของเว็บแอป บอกรายละเอียดรีเควส
-const app = express();
-const port = 3000; //กำหนด port ที่จะใช้รันแอป
+
+const port = 3000; //กำหนด port ที่จะใช้รันแอป 
+
+import path from 'path'; //สร้าง path
+const __dirname = path.resolve(); //เพิ่มจากเดิม
+
 
 app.use(morgan('combined'));
+app.use(express.static(path.join(__dirname,"/public"))); //public เก็บพวก static file เช่น รูป, html, css
 
+//กำหนดpathแล้วทำให้ไปที่pathก่อนเลย(ไม่มาเข้าapp.getแล้ว)
 app.get("/",(req,res)=>{
     res.send("Hello MYN"); //คำสั่งที่จะตอบกลับ
 }) //ใช้จัดการรีเควสที่เข้ามาหน้า / จะส่งข้อมูลอะไรกลับไป
